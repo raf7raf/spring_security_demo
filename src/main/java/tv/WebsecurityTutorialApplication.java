c4d6b5a1a0e5be5bf9221e1f1039af37a22d1be7
Rafal Frackowiak <rfrackowiak@sis.tv>
2016-10-29 22:59:35 +0100

    init

---
+++ src/main/java/tv/WebsecurityTutorialApplication.java
@@ -0,0 +1,12 @@
+package tv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsecurityTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsecurityTutorialApplication.class, args);
+	}
+}
